a2ab29b9d6911b61d9b6da5e9b6b68b796860cf6
Kaugmz <jessica.saqueto@escola.pr.gov.br>
2026-06-08 15:21:13 +0000

    .

---
+++ procurando-alunos.js
@@ -0,0 +1,19 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10,8,7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno){
+    if (listaDeAlunosEMedias[0].includes(aluno)){
+        console.log(`${aluno} está cadastro`)
+
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+
+        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+
+        console.log(mediaDoAluno);
+    }else{
+        console.log("Aluno não encontrado!");
+    }
+}
+exibeNomeENota("João")
